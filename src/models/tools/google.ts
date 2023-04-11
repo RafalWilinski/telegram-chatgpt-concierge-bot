19b9ae7727494c03c93177bbab2f0e8fbba07b3b
Rafal Wilinski <raf.wilinski@gmail.com>
2023-04-11 19:37:12 +0200

    Add try/catch to google tool

--- src/models/tools/google.ts
+++ src/models/tools/google.ts
@@ -6,20 +6,25 @@ export const googleTool = new DynamicTool({
   description:
     "This is Google. Use this tool to search the internet. Input should be a string",
   func: async (searchPhrase: string) => {
-    const response = await google.search(searchPhrase, {
-      page: 0,
-      safe: false, // Safe Search
-      parse_ads: false, // If set to true sponsored results will be parsed
-      additional_params: {
-        // add additional parameters here, see https://moz.com/blog/the-ultimate-guide-to-the-google-search-parameters and https://www.seoquake.com/blog/google-search-param/
-      },
-    });
+    try {
+      const response = await google.search(searchPhrase, {
+        page: 0,
+        safe: false, // Safe Search
+        parse_ads: false, // If set to true sponsored results will be parsed
+        additional_params: {
+          // add additional parameters here, see https://moz.com/blog/the-ultimate-guide-to-the-google-search-parameters and https://www.seoquake.com/blog/google-search-param/
+        },
+      });
 
-    console.log({ googleResponse: response });
+      console.log({ googleResponse: response });
 
-    return JSON.stringify({
-      results: response.results,
-      featured: response.featured_snippet,
-    });
+      return JSON.stringify({
+        results: response.results,
+        featured: response.featured_snippet,
+      });
+    } catch (error) {
+      console.log(error);
+      return "Failed to get results from Google. Do not try using Google again.";
+    }
   },
 });
